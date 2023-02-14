67f4eefdd2907727d3343c8c579d06c4fb3dd104
victro-nuzhniy <nuzhniyva@gmail.com>
2023-02-14 16:12:54 +0200

    commerce added media to urls

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -17,17 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from e_commerce import settings
 from shop.views import page_not_found
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
 ]
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#      path('__debug__/', include(debug_toolbar.urls)),
-#     ] + urlpatterns
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    # urlpatterns = [
+    #  path('__debug__/', include(debug_toolbar.urls)),
+    # ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
