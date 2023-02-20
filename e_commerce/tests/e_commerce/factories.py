5ef7644455b9eeeb87249c3ec681559703c0fe3b
victro-nuzhniy <nuzhniyva@gmail.com>
2023-02-20 16:48:41 +0200

    With_tests added tests to test_tables.py

--- e_commerce/tests/e_commerce/factories.py
+++ e_commerce/tests/e_commerce/factories.py
@@ -1,13 +1,52 @@
 import factory
+from slugify import slugify
 
-from shop.models import SuperCategory
+from shop.models import (
+    SuperCategory,
+    Category,
+    CategoryFeatures,
+)
 from tests.bases import BaseModelFactory
 
 
 class SuperCategoryFactory(BaseModelFactory):
     class Meta:
         model = SuperCategory
+        exclude = ('category_set',)
 
-    name = factory.Faker('pystr', max_chars=50)
+    name = factory.Faker('pystr', min_chars=1, max_chars=50)
     icon = factory.django.ImageField(color="blue")
+    category_set = factory.RelatedFactoryList(
+        factory='tests.e_commerce.factories.CategoryFactory',
+        factory_related_name='category_set',
+        size=0,
+    )
+
+
+class CategoryFactory(BaseModelFactory):
+    class Meta:
+        model = Category
+        django_get_or_create = ('slug', 'super_category')
+        exclude = ('category_feature_set', 'seq')
+
+    seq = factory.Sequence(lambda x: x)
+    name = factory.Faker('pystr', min_chars=1, max_chars=100)
+    slug = factory.LazyAttribute(function=lambda obj: slugify(str(obj.name)))
+    super_category = factory.SubFactory(factory=SuperCategoryFactory)
+    icon = factory.django.ImageField(color='yellow')
+    category_feature_set = factory.RelatedFactoryList(
+        factory='tests.e_commerce.factories.CategoryFeatureFactory',
+        factory_related_name='category_feature_set',
+        size=0,
+    )
+
+
+class CategoryFeatureFactory(BaseModelFactory):
+    class Meta:
+        model = CategoryFeatures
+        django_get_or_create = ('category',)
+
+    category = factory.SubFactory(factory=CategoryFactory)
+    feature_name = factory.Faker('pystr', min_chars=1, max_chars=100)
+
 
--- e_commerce/tests/e_commerce/test_tables.py
+++ e_commerce/tests/e_commerce/test_tables.py
@@ -1,8 +1,11 @@
 import pytest
 
-from shop.models import SuperCategory
+from shop.models import SuperCategory, Category
 from tests.bases import BaseModelFactory
-from tests.e_commerce.factories import SuperCategoryFactory
+from tests.e_commerce.factories import (
+    SuperCategoryFactory,
+    CategoryFactory,
+)
 
 
 @pytest.mark.django_db
@@ -16,5 +19,20 @@ class TestSuperCategory:
 
     def test__str__(self) -> None:
         obj: SuperCategory = SuperCategoryFactory()
+        expected_result = obj.name
+        assert expected_result == obj.__str__()
+
+
+@pytest.mark.django_db
+class TestCategory:
+    pytestmark = pytest.mark.django_db
+
+    def test_factory(self) -> None:
+        BaseModelFactory.check_factory(
+            factory_class=CategoryFactory, model=Category
+        )
+
+    def test__str__(self) -> None:
+        obj: Category = CategoryFactory()
         expected_result = obj.name
         assert expected_result == obj.__str__()
