3b5e702cd5209fcc1c20fcdb0607dbf2f0deaf9d
victro-nuzhniy <nuzhniyva@gmail.com>
2023-02-20 11:41:00 +0200

    interm_deploy deleted tests

--- e_commerce/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import random
-
-
-@pytest.fixture(scope="function", autouse=True)
-def faker_seed() -> None:
-    """Generate random seed for Faker instance."""
-    return random.seed(version=3)
