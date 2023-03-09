601551185e033b89a78c1b81abffb4ee4f2535e2
victro-nuzhniy <nuzhniyva@gmail.com>
2023-03-09 14:59:26 +0200

    new_intermidiate_deploy deleted tests, changed database settings

--- e_commerce/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import random
-
-import pytest
-
-
-@pytest.fixture(scope="function", autouse=True)
-def faker_seed() -> None:
-    """Generate random seed for Faker instance."""
-    return random.seed(version=3)
