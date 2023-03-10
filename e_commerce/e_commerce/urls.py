a3ae60d845908fddcc0cb03d362e267d5b734822
victro-nuzhniy <nuzhniyva@gmail.com>
2023-03-10 23:03:26 +0200

    with_tests added newly created AdminLogin path to e_commerce/urls, deleted translit data in login.html

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from shop.views import page_not_found
+from shop.views import page_not_found, AdminLoginView
 
 from e_commerce import settings
 
 urlpatterns = [
     path("grappelli/", include("grappelli.urls")),
+    path('admin/login/', AdminLoginView.as_view(), name='login'),
     path("admin/", admin.site.urls),
     path("", include("shop.urls")),
 ]
